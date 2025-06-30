16b4b6f6b33c268cf11306e89dad033a7d4a1356
Roman Kostiv <rkostiv@gmail.com>
2025-06-30 14:14:34 +0300

    Solution

--- author/serializers.py
+++ author/serializers.py
@@ -6,11 +6,4 @@ from author.models import Author
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
-        fields = [
-            "id",
-            "first_name",
-            "last_name",
-            "pseudonym",
-            "age",
-            "retired"
-        ]
+        fields = "__all__"
