8ed86cb45c32f7190f6d92a75929d21b05fc177b
Roman Kostiv <rkostiv@gmail.com>
2025-06-30 14:02:29 +0300

    Solution

--- author/serializers.py
+++ author/serializers.py
@@ -3,40 +3,14 @@ from rest_framework import serializers
 from author.models import Author
 
 
-class AuthorSerializer(serializers.Serializer):
-
-    id = serializers.IntegerField(read_only=True)
-    first_name = serializers.CharField(max_length=255)
-    last_name = serializers.CharField(max_length=255)
-    pseudonym = serializers.CharField(max_length=255)
-    age = serializers.IntegerField()
-    retired = serializers.BooleanField()
-
-    def create(self, validated_data):
-        return Author.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.first_name = validated_data.get(
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = [
+            "id",
             "first_name",
-            instance.first_name
-        )
-        instance.last_name = validated_data.get(
             "last_name",
-            instance.last_name
-        )
-        instance.pseudonym = validated_data.get(
             "pseudonym",
-            instance.pseudonym
-        )
-        instance.age = validated_data.get(
             "age",
-            instance.age
-        )
-        instance.retired = validated_data.get(
-            "retired",
-            instance.retired
-        )
-
-        instance.save()
-
-        return instance
+            "retired"
+        ]
