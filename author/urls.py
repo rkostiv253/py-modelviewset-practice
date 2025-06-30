161cfc112b38b81de0ebf8e2bd2b3c6dd0c09d9a
Roman Kostiv <rkostiv@gmail.com>
2025-06-30 14:08:30 +0300

    Solution

--- author/urls.py
+++ author/urls.py
@@ -4,7 +4,7 @@ from rest_framework import routers
 from author.views import AuthorViewSet
 
 router = routers.DefaultRouter()
-router.register("authors", AuthorViewSet)
+router.register("authors", AuthorViewSet, basename="manage")
 
 urlpatterns = [
     path("", include(router.urls))
